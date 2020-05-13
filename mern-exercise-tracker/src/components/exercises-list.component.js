1de2f48d2eee990baf738328f23dfba12500e3a3
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-05-13 20:19:30 +0300

    react components

---
+++ mern-exercise-tracker/src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ExercisesList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Exercises List</h1>
+      </div>
+    );
+  }
+}
