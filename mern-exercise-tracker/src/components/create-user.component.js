741a947928d5e1243aae8e3e9ff9e0447401964d
Tatjana Svjatova <tsv.mailbox@gmail.com>
2020-05-14 14:36:45 +0300

    connecting FE to BE

--- mern-exercise-tracker/src/components/create-user.component.js
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class CreateUser extends Component {
   constructor(props) {
@@ -25,6 +26,9 @@ export default class CreateUser extends Component {
     };
 
     console.log(user);
+
+    axios.post("http://localhost:5000/users/", user).then((res) => console.log(res.data));
+
     this.setState({
       username: "",
     });
